d96b35eaf1a17139a4f0e29a649ba149364f39c0
muhammadmuzakky161006 <muhammadmuzakky161006@gmail.com>
2026-03-29 01:21:05 +0700

    Update User.php

--- User.php
+++ User.php
@@ -1,9 +1,9 @@
 <?php
 class User {
-    public $firstName;
-    public $lastName;
-    public $phoneNumber;
-    public $address;
+    private $firstName;
+    private $lastName;
+    private $phoneNumber;
+    private $address;
 
     public function __construct($fName, $lName, $phone, $addr) {
         $this->firstName = htmlspecialchars($fName);
@@ -14,6 +14,22 @@ class User {
 
     public function getFullName() {
         return $this->firstName . " " . $this->lastName;
+    }
+
+    public function getPhoneNumber() {
+        return $this->phoneNumber;
+    }
+
+    public function getAddress() {
+        return $this->address;
+    }
+
+    public function getUserInfo() {
+        return [
+            "nama" => $this->getFullName(),
+            "phone" => $this->phoneNumber,
+            "alamat" => $this->address
+        ];
     }
 }
 ?>
